3a8e23f6d6c6e12159a9c46fe4b8fe5101f9aa81
GueroGuero <joelrdzj96@gmail.com>
2023-05-17 22:05:50 -0500

    saving work

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,6 +24,42 @@ with open(file, encoding="utf-8") as csvfile:
     #loop through profits for calculation of average change
     for i in range(len(total_amount)-1):
         average_change.append(total_amount[i+1]-total_amount[i])
+
 #variables for greatest incear and decrease
 greatest_increase = max(average_change)
 greatest_decrease = min(average_change)
+
+#greatest increase and decrease added to months
+#also adding 1 to show max and min after month calculation
+greatest_increase_month = average_change.index(max(average_change)) + 1
+greatest_decrease_month = average_change.index(min(average_change)) + 1
+
+#print analysis
+print("Financial Analysis")
+print("-----------------------")
+print(f"Total Months : {len(total_months)}")
+print(f"Total : ${sum(total_amount)}")
+
+#calculate average change
+print(f"Average Change : ${sum(average_change)/len(average_change)}")
+
+#make greatest increase and decrease values as strings and add signs and months
+print(f"Greatest Increase in Profits: {total_months[greatest_increase_month]} (${(str(greatest_increase))})")
+print(f"Greatest Decrease in Profits: {total_months[greatest_decrease_month]} (${(str(greatest_decrease))})")
+
+#make text file
+analysis = os.path.join("Analysis", "FinancialAnalysis.txt")
+with open(analysis, "w") as file:
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("-------------------")
+    file.write("\n")
+    file.wrtie(f"Total Months : {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total : ${sum(total_amount)}")
+    file.write("\n")
+    file.write(f"Average Change : ${sum(average_change)/len(average_change)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[greatest_increase_month]} (${(str(greatest_increase))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[greatest_decrease_month]} (${(str(greatest_decrease))})")
